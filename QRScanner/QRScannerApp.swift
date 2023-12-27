16bd029910490f383d1db675ffbcd97ff1ad7ddb
Shebin <shebin.pk@fingent.com>
2023-12-27 12:44:42 +0530

    Initial Commit

---
+++ QRScanner/QRScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRScannerApp.swift
+//  QRScanner
+//
+//  Created by Shebin PK on 27/12/23.
+//
+
+import SwiftUI
+
+@main
+struct QRScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
